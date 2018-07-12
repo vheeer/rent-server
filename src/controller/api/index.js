3dceccdbc02f031248584fade0a2dbcc14ff7ec8
vheeer <vheeer@126.com>
2018-07-12 21:31:53 +0800

    delete api/index.js

--- src/controller/api/index.js
+++
@@ -1,8 +0,0 @@
-const Base = require('./base.js');
-
-module.exports = class extends Base {
-    indexAction() {
-        console.log("api/index/indexAction");
-        return this.success("all right");
-    }
-};
