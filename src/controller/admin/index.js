9808684fab6a246a8aa38f5ece5f5ecc406ecfcb
vheeer <vheeer@126.com>
2018-07-15 09:32:09 +0800

    change index.js to clientt_1

--- src/controller/admin/index.js
+++ src/controller/admin/index.js
@@ -5,6 +5,5 @@ module.exports = class extends Base {
     console.log("admin/index/indexAction");
     return this.success("all right client_1");
     
-    return this.success("all right server_3");
   }
 };
